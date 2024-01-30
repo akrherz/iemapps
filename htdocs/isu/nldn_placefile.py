8b38a76423e5d4cf2615b74030d9f04cd018046d
akrherz <akrherz@iastate.edu>
2024-01-29 21:29:28 -0600

    feat: control timestamp to seconds

--- htdocs/isu/nldn_placefile.py
+++ htdocs/isu/nldn_placefile.py
@@ -1,4 +1,5 @@
 """Generate a NLDN Gibson Ridge Placefile for ISU research usage."""
+from datetime import datetime, timezone
 
 from pyiem.webutil import iemapp
 
@@ -19,11 +20,18 @@ def gen_strikes(cursor, sts, ets):
         )
 
 
-@iemapp(iemdb="nldn", iemdb_cursorname="nldn")
+@iemapp(iemdb="nldn", parse_times=False, iemdb_cursorname="nldn")
 def application(environ, start_response):
     """WSGI application."""
-    sts = environ["sts"]
-    ets = environ["ets"]
+    fmt = (
+        "%Y-%m-%dT%H:%M:%SZ"
+        if len(environ["sts"]) == 20
+        else "%Y-%m-%dT%H:%MZ"
+    )
+    sts = datetime.strptime(environ["sts"], fmt)
+    sts = sts.replace(tzinfo=timezone.utc)
+    ets = datetime.strptime(environ["ets"], fmt)
+    ets = ets.replace(tzinfo=timezone.utc)
     content = f"""
 Title: NLDN {sts} to {ets}
 Color: 255 255 255
